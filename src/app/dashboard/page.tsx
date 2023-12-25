f0f7293a51672a1485d952eba0b3041813916ef9
jordwan <94031022+jordwan@users.noreply.github.com>
2023-12-24 20:58:27 -0800

    dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,30 +1,3 @@
 export default function Dashboard() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Hello World. <br />
-        under construction. open source. contribute on github.
-        <br />
-        <br />
-        actions/
-        <br />
-        .auth.ts
-        <br />
-        .search.ts
-        <br />
-        .order.ts
-        <br />
-        <br />
-        app/
-        <br />
-        /someroute/
-        <br />
-        .page.tsx
-        <br />
-        /api/
-        <br />
-        ./apiroute/
-      </div>
-    </main>
-  );
+  return <></>;
 }
