bae6d35fe93034fe2edbaf733b32d4901cfddecd
jordwan <94031022+jordwan@users.noreply.github.com>
2023-12-24 18:19:11 -0800

    prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "FootyFiEND NEXT",
-  description: "The Future of Snowboarding",
+  title: 'FootyFiEND NEXT',
+  description: 'The Future of Snowboarding',
 };
 
 export default function RootLayout({
