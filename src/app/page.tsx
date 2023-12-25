7d31a04906926d077124b5815de3207443b1fcad
jordwan <94031022+jordwan@users.noreply.github.com>
2023-12-24 19:53:53 -0800

    page changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,28 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Hello World
+        Hello World. <br />
+        under construction. open source. contribute on github.
+        <br />
+        <br />
+        actions/
+        <br />
+        .auth.ts
+        <br />
+        .search.ts
+        <br />
+        .order.ts
+        <br />
+        <br />
+        app/
+        <br />
+        /someroute/
+        <br />
+        .page.tsx
+        <br />
+        /api/
+        <br />
+        ./apiroute/
       </div>
     </main>
   );
